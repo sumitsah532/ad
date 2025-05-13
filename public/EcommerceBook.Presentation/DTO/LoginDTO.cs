c2900a70e04297036b9ca54fcab3a14958032c12
Sumit Kumar Sah <71826572+sumitsah532@users.noreply.github.com>
2025-05-13 23:47:35 +0545

    for the login and user

---
+++ public/EcommerceBook.Presentation/DTO/LoginDTO.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceBook.Presentation.DTO
+{
+    public class LoginDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+}
