16735f5831f294d9087ffbc2190ba79af6d7904e
Mohammad Taukir Reza <jackhart3312@gmail.com>
2025-05-14 00:44:38 +0545

    extend IBookRepository with no-tracking retrieval and detached update methods

---
+++ EcommerceBook.Domain/IRepository/IBookRepository.cs
@@ -0,0 +1,20 @@
+﻿using EcommerceBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EcommerceBook.Domain.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<Book> AddAsync(Book book);
+        Task<Book> UpdateAsync(Book book);
+        Task<bool> DeleteAsync(Guid id);
+        Task<IEnumerable<Book>> GetAllAsync();
+        Task<Book> GetByIdAsync(Guid id);
+
+        // New methods for detached entity updates
+        Task<Book> GetByIdAsNoTrackingAsync(Guid id);
+        Task<Book> UpdateDetachedAsync(Book book);
+    }
+}
