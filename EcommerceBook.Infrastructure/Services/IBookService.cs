62ad00bd62bfd230920b4a8176cd6ffadf4f816d
Mohammad Taukir Reza <jackhart3312@gmail.com>
2025-05-14 00:02:48 +0545

    Add IBookService interface with CRUD method definitions

---
+++ EcommerceBook.Infrastructure/Services/IBookService.cs
@@ -0,0 +1,22 @@
+﻿using EcommerceBook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace EcommerceBook.Application.IServices
+{
+    public interface IBookService
+    {
+        // Add a new book
+        Task<Book> AddBook(Book book);
+
+        // Update an existing book
+        Task<Book> UpdateBook(Book book);
+
+        // Delete a book by its ID
+        Task<bool> DeleteBook(Guid id);
+
+       
+        Task<Book?> UpdateBookDetached(Book book);
+    }
+}
